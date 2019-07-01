6151e057b1db6d7b277fdc27b0d7fb21f713431a
elenore29 <elenore.nava@gmail.com>
2019-07-01 11:47:05 -0500

    Suiendo evento click de botones que llevan al mapa de cada restaurante

--- data.js
+++ data.js
@@ -2,5 +2,35 @@ window.data = {
     filterByName: (arr, name) => {
         const filteringName = arr.filter(element => (element.name.toLowerCase().match(name.toLowerCase())));
         return filteringName;
+    },
+    sortByRating: (arr, order) => {
+         const sortRating = arr.sort((a, b) => {
+            if(a.rating > b.rating){
+                return 1*order; 
+            } else if(b.rating > a.rating) {
+                return -1*order; 
+            } else {
+                return 0; 
+            }
+        });
+        return sortRating; 
+    },
+    sortByName: (arr, order) => {
+        const sortName = arr.sort((a, b) => {
+            if(a.name > b.name){
+                return 1*order; 
+            }
+            else if(b.name > a.name) {
+                return -1*order;
+            } else {
+                return 0; 
+            }
+        })
+        return sortName; 
+    },
+    find: (arr) =>{
+        arr.forEach(element => {
+            console.log(element.id)
+        });
     }
 }
--- main.js
+++ main.js
@@ -14,7 +14,7 @@ fetch(url)
     })
     .catch(err => (err))
 
-var map; 
+var map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -35,34 +35,103 @@ var marker = new google.maps.Marker({
 
 
 const summary = document.getElementById('summary');
+const locationAddress = document.getElementsByClassName('location'); 
 
 const printData = (newData) => {
     summary.innerHTML = ""; 
     newData.forEach(element => {
-    const result = `<div class="element-box">
-    <div class="restaurant">${element.name}</div>
-    <div class="info">
-    <p>${element.address.state}</p>
-    <p>${element.address.city}</p>
-    <p>Rating: ${element.rating}</p>
-    </div>
-    </div>`;
-    summary.insertAdjacentHTML("beforeend", result);
+        const result = 
+        `<div class="element-box">
+            <div class="restaurant">${element.name}
+                <p>Rating: ${element.rating}</p>
+            </div>
+            <div class="info">
+                <p>Dirección:<p>
+                <p>${element.address.state}</p>
+                <p>${element.address.city}</p>
+                <p>${element.address.street}</p>
+                <button id="${element.id}" class="location">Ver en el mapa</button>
+            </div>
+            <div class="info">
+                <p>Telefono: ${element.contact.phone}</p>
+                <p>E-mail: <a href="mailto:${element.contact.email}">${element.contact.email}<a></p>
+                <button class="link-web"> 
+                <a class="link-web" href="${element.contact.site}" target="_blank">Sitio web</a>
+                </button>
+            </div>
+        </div>`;
+        summary.insertAdjacentHTML("beforeend", result);
+
+      
 });
-}
+for(let i = 0; locationAddress.length; i++){
+    locationAddress[i].addEventListener('click', () => {
+        newData.forEach(element => {
+        
+            if(locationAddress[i].id == element.id){
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: {
+                        lat: element.address.location.lat,
+                        lng: element.address.location.lng
+                    },
+                    zoom: 16,
+                });
+                
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: element.address.location.lat,
+                    lng: element.address.location.lng
+                },
+                map: map
+            });  
+            }
+        })
+       
+       
+    }); 
+};
+};
 
 const search = document.getElementById('search'); 
 search.addEventListener('keyup', ()=>{
     let name = search.value;
     const newData = JSON.parse(localStorage.getItem('data'));
     let filtered = window.data.filterByName(newData, name); 
-    console.log(filtered); 
     if (name == "") {
-        console.log('algo')
         printData(newData);
       } else {
-          console.log('otra cosa')
         printData(filtered);
       }
-    console.log(name);
+})
+
+const ratingDes = document.getElementById('rating-des');
+
+ratingDes.addEventListener('click', () => {
+    const newData = JSON.parse(localStorage.getItem('data'));
+    let sorter = window.data.sortByRating(newData, -1);
+    printData(sorter); 
+}); 
+
+const ratingAsc = document.getElementById('rating-asc');
+
+ratingAsc.addEventListener('click', () => {
+    const newData = JSON.parse(localStorage.getItem('data'));
+    let sorter = window.data.sortByRating(newData, 1);
+    printData(sorter); 
+}); 
+
+
+const abcDes = document.getElementById('abc-des');
+
+abcDes.addEventListener('click', () => {
+    const newData = JSON.parse(localStorage.getItem('data'));
+    printData(newData); 
+}); 
+
+const abcAsc = document.getElementById('abc-asc');
+
+abcAsc.addEventListener('click', () => {
+    const newData = JSON.parse(localStorage.getItem('data'));
+    let sorter = window.data.sortByName(newData, -1);
+    printData(sorter); 
 })
